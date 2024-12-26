3a8aa48600cb34f6e94feec40f62ae2d6be74a21
walsons <walsons@163.com>
2024-12-26 22:10:31 +0800

    optimize evaluation of Pawn (#47)

--- src/evaluate.h
+++ src/evaluate.h
@@ -1,6 +1,8 @@
 #ifndef EVALUATE_H
 #define EVALUATE_H
 
+#include <vector>
+
 #include "position.h"
 #include "bitboard.h"
 
@@ -75,12 +77,56 @@ private:
         return OneCount(position.Pieces(Color::RED, pt)) * PieceValue<pt> - OneCount(position.Pieces(Color::BLACK, pt)) * PieceValue<pt>;
     }
 };
-// Pawn that across to river gain double score
+// Pawn that across to river gain double score, 
+// gain more score near enemy king(prefer vertical)
+// gain more score side by side with other friend pawn
 template <>
 inline int Evaluate::ScoreCalculator<PieceType::PAWN>(const Position& position)
 {
+    int score = 0;
     Bitboard redPawnBB = position.Pieces(Color::RED, PieceType::PAWN);
     Bitboard blackPawnBB = position.Pieces(Color::BLACK, PieceType::PAWN);
+
+    std::vector<Square> redPawnSquares;
+    redPawnSquares.reserve(5);
+    auto tmp = redPawnBB;
+    while (tmp)
+    {
+        Square ps = PopLSB(tmp);
+        Square ks = position.KingSquare(Color::BLACK);
+        int v = std::abs(ps / 9 - ks / 9);
+        int h = std::abs(ps % 9 - ks % 9);
+        score -= (2 * v + h);
+        redPawnSquares.push_back(ps);
+    }
+    // set first pawn as origin
+    int redDistance = 0;
+    for (size_t i = 1; i < redPawnSquares.size(); ++i)
+    {
+        redDistance += Distance(redPawnSquares[0], redPawnSquares[i]);
+    }
+    score -= redDistance;
+
+    std::vector<Square> blackPawnSquares;
+    blackPawnSquares.reserve(5);
+    tmp = blackPawnBB;
+    while (tmp)
+    {
+        Square ps = PopLSB(tmp);
+        Square ks = position.KingSquare(Color::RED);
+        int v = std::abs(ps / 9 - ks / 9);
+        int h = std::abs(ps % 9 - ks % 9);
+        score += 2 * (10 - v) + (10 - h);
+        blackPawnSquares.push_back(ps);
+    }
+    // set first pawn as origin
+    int blackDistance = 0;
+    for (size_t i = 1; i < redPawnSquares.size(); ++i)
+    {
+        blackDistance += Distance(blackPawnSquares[0], blackPawnSquares[i]);
+    }
+    score += blackDistance;
+
     return ((OneCount(redPawnBB & BlackRegion) * 2 + OneCount(redPawnBB & RedRegion)) - (OneCount(blackPawnBB & RedRegion) * 2 + OneCount(blackPawnBB & BlackRegion)))
            * PieceValue<PieceType::PAWN>;
 }
