557111c7df106b9e0b689184b7cd8dbf426c63e3
walsons <walsons@163.com>
2025-01-11 15:52:52 +0800

    update evaluation to motivate knight move from origin position, refactor test (#65)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,30 +1,5 @@
 #include "evaluate.h"
 
-constexpr int GameStartRookMoveBonus[90] = {
-    0, 20, 0, 0, 0, 0, 0, 20, 0,
-    10, 20, 0, 0, 0, 0, 0, 20, 10,
-    10, 20, 0, 0, 0, 0, 0, 20, 10,
-    0, 20, 0, 0, 0, 0, 0, 20, 0,
-    0, 30, 0, 50, 0, 50, 0, 30, 0,
-    0, 30, 0, 50, 0, 50, 0, 30, 0,
-    0, 20, 0, 0, 0, 0, 0, 20, 0,
-    10, 20, 0, 0, 0, 0, 0, 20, 10,
-    10, 20, 0, 0, 0, 0, 0, 20, 10,
-    0, 20, 0, 0, 0, 0, 0, 20, 0,
-};
-
-constexpr int GameStartKnightMoveBonus[90] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    10, 0, 10, 0, 0, 0, 10, 0, 10,
-    0, 0, 0, 0, 20, 0, 0, 0, 0,
-    0, 10, 0, 20, 0, 20, 0, 1, 0,
-    0, 10, 0, 20, 0, 20, 0, 1, 0,
-    0, 0, 0, 0, 20, 0, 0, 0, 0,
-    10, 0, 10, 0, 0, 0, 10, 0, 10,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-};
 
 Value Evaluate::Eval(const Position& position)
 {
@@ -43,67 +18,19 @@ Value Evaluate::Eval(const Position& position)
     // Piece in enemy region gain extra 10 score
     score += OneCount(position.Pieces(Color::RED) & BlackRegion) * 10;
     score -= OneCount(position.Pieces(Color::BLACK) & RedRegion) * 10;
-    // Enemy has two advisor, increase own cannon value
-    // if (OneCount(position.Pieces(Color::BLACK, PieceType::ADVISOR)) == 2)
-    //     score += OneCount(position.Pieces(Color::RED, PieceType::CANNON) * 30);
-    // if (OneCount(position.Pieces(Color::RED, PieceType::ADVISOR)) == 2)
-    //     score += OneCount(position.Pieces(Color::BLACK, PieceType::CANNON) * 30);
 
-    // In order to encourage rook and knight move forward
-    // if (position.piece_count() > 28)
-    // {
-    //     Bitboard rbb = position.Pieces(Color::RED, PieceType::ROOK);
-    //     while (rbb)
-    //     {
-    //         score += GameStartRookMoveBonus[NUM(PopLSB(rbb))];
-    //     }
-    //     rbb = position.Pieces(Color::BLACK, PieceType::ROOK);
-    //     while (rbb)
-    //     {
-    //         score -= GameStartRookMoveBonus[NUM(PopLSB(rbb))];
-    //     }
-    //     Bitboard nbb = position.Pieces(Color::RED, PieceType::KNIGHT);
-    //     while (nbb)
-    //     {
-    //         score += GameStartKnightMoveBonus[NUM(PopLSB(nbb))];
-    //     }
-    //     nbb = position.Pieces(Color::BLACK, PieceType::KNIGHT);
-    //     while (nbb)
-    //     {
-    //         score -= GameStartKnightMoveBonus[NUM(PopLSB(nbb))];
-    //     }
-    // }
-
-
-    // 3. Cannon and enemy king in the same line, gain extra 50 score
-    // Bitboard redCannonB = position.Pieces(Color::RED, PieceType::CANNON);
-    // Bitboard blackKingB = position.Pieces(Color::BLACK, PieceType::KING);
-    // Square ks = PopLSB(blackKingB);
-    // File kf = FileOf(ks);
-    // Rank kr = RankOf(ks);
-    // while (redCannonB)
-    // {
-    //     Square s = PopLSB(redCannonB);
-    //     if (FileOf(s) == kf || RankOf(s) == kr)
-    //     {
-    //         score += 50;
-    //         break;
-    //     }
-    // }
-    // Bitboard blackCannonB = position.Pieces(Color::BLACK, PieceType::CANNON);
-    // Bitboard redKingB = position.Pieces(Color::RED, PieceType::KING);
-    // ks = PopLSB(redKingB);
-    // kf = FileOf(ks);
-    // kr = RankOf(ks);
-    // while (blackCannonB)
-    // {
-    //     Square s = PopLSB(blackCannonB);
-    //     if (FileOf(s) == kf || RankOf(s) == kr)
-    //     {
-    //         score -= 50;
-    //         break;
-    //     }
-    // }
+    // In order to encourage knight to move from initial position
+    if (position.piece_count() > 26)
+    {
+        if (position.piece_from_square(SQ_B0) != Piece::W_KNIGHT)
+            score += 10;
+        if (position.piece_from_square(SQ_H0) != Piece::W_KNIGHT)
+            score += 10;
+        if (position.piece_from_square(SQ_B9) != Piece::B_KNIGHT)
+            score -= 10;
+        if (position.piece_from_square(SQ_H9) != Piece::B_KNIGHT)
+            score -= 10;
+    }
 
     return position.side_to_move() == Color::RED ? score : -score;
 }
